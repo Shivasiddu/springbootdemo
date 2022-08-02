1cda06f3472113ffe8bcf75d00e9694a8a0119eb
Shivasiddu <105356809+Shivasiddu@users.noreply.github.com>
2022-08-02 15:09:22 +0530

    Add files via upload

---
+++ 25-Olx-Masters-Microservice/src/main/java/com/zensar/OlxMastersApplication.java
@@ -0,0 +1,12 @@
+package com.zensar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlxMastersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OlxMastersApplication.class, args);
+	}
+}
